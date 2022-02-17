9de34fa41b4c1113ba1a81c6d5eba00cf10a347f
fernando3600 <fantoniogonzalez1999@gmail.com>
2022-02-17 15:25:11 -0600

    primer commit

---
+++ app/src/main/java/com/example/dm_t1_5_todo_junto_funciona/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.dm_t1_5_todo_junto_funciona;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageButton btn = findViewById(R.id.searchImageButton);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), SecondActivity.class);
+                startActivity(intent );
+            }
+        });
+    }
+}
